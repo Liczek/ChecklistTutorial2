99e8d3c5e60fccc60a45c26a455ebed4b1fa863b
Paweł Liczmański <paweliczek@gmail.com>
2017-02-18 12:59:22 +0100

    02- Arrays s.58

---
+++ ChecklistTutorial2/ChecklistTutorial2/ChecklistItem.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistItem.swift
+//  ChecklistTutorial2
+//
+//  Created by Paweł Liczmański on 18.02.2017.
+//  Copyright © 2017 Paweł Liczmański. All rights reserved.
+//
+
+import Foundation
+class ChecklistItem {
+    var text = ""
+    var checked = false
+
+    func toggleChecked() {
+        checked = !checked
+    }
+}
